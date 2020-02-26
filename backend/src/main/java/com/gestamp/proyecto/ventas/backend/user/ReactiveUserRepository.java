30e313dade8194107d178dbcb8c4ecfdc3733afc
alexelfandi <alexelfandi60@gmail.com>
2020-02-26 17:45:13 +0100

    Revert "Merge branch 'alvaro' into alex"
    This reverts commit 1e8dbb807bdab48e6ead252543817b78915581c3, reversing
    changes made to 329323b4644221c9a129755c419f6c0a5589b6e8.

--- backend/src/main/java/com/gestamp/proyecto/ventas/backend/user/ReactiveUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gestamp.proyecto.ventas.backend.user;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReactiveUserRepository extends ReactiveMongoRepository <User, String> {
-
-
-}
