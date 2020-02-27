25e092eaad654c73dd25f621601f7f54d5c3b452
JonCas22 <joncaso22@gmail.com>
2020-02-27 19:38:42 +0100

    fixed update-date

--- frontend/src/app/update-modal/update-modal.component.ts
+++ frontend/src/app/update-modal/update-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, SimpleChange } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Order } from '../models/order';
 import { VentasService } from '../services/ventas.service';
@@ -11,6 +11,10 @@ import { VentasService } from '../services/ventas.service';
 export class UpdateModalComponent implements OnInit {
 
   order:Order = new Order();
+  dateToShow;
+  dateToShow2;
+  dateObject:any = {};
+
 
   constructor(
     public dialogRef: MatDialogRef<UpdateModalComponent>,
@@ -25,6 +29,19 @@ export class UpdateModalComponent implements OnInit {
     console.log(this.data.item);
     this.order = this.data.item;
     //this.order = this.data.item;
+    this.getDate();
+  }
+
+  getDate(){
+    this.dateToShow = new Date(this.order.OrderDate).toISOString().slice(0, 10);
+    this.dateToShow2 = new Date(this.order.ShipDate).toISOString().slice(0, 10);
+
+    //this.order.OrderDate = this.dateToShow;
+    console.log(this.dateToShow);
+    this.dateObject.orderDate = this.dateToShow;
+    this.dateObject.shipDate = this.dateToShow2;
+    
+    
   }
 
   onSubmit(form) {
@@ -36,6 +53,11 @@ export class UpdateModalComponent implements OnInit {
       
       
     });
+  }
+
+  doSomething(){
+    console.log(this.order.OrderDate);
+    
   }
 
   closeModal(){
